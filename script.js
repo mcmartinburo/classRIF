b6d840c29aa0c000f34ca0a0b95a1df8805e3b1b
MC Martín-Buro <42032448+mcmartinburo@users.noreply.github.com>
2026-01-19 13:02:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -84,7 +84,9 @@ function procesar() {
   const condicionFinal = Object.keys(resultados).reduce((a,b) => resultados[a]>=resultados[b]?a:b);
   document.getElementById("condicionFinal").innerText = "Condición predominante: " + condicionFinal;
 
+  // PRIMERO: generar tabla
   generarTablaResultados(resultados, totalItems);
+  // SEGUNDO: dibujar gráfica debajo
   dibujarGrafica(resultados, totalItems);
 }
 
@@ -144,9 +146,7 @@ function dibujarGrafica(resultados, totalItems) {
             display:true,
             text:"% de recuerdo"
           },
-          ticks:{
-            stepSize:10
-          }
+          ticks:{ stepSize:10 }
         }
       }
     }
